db434cbe657f75015b6ff50cf28f1d003fc4ace2
Divyansh Singh <divyansh205044@gmail.com>
2026-07-08 23:49:31 +0530

    changed indent

--- ARRAYS/05_RemoveDupli.cpp
+++ ARRAYS/05_RemoveDupli.cpp
@@ -2,43 +2,36 @@
 #include<set>
 using namespace std;
 
-int Brute(int arr[], int n)
-{
-    set<int> st;                 // initialized set = that contains Unique elements
+int Brute(int arr[], int n){
+    set<int> st;               
 
-    for(int i=0;i<n;i++)
-    {
+    for(int i=0;i<n;i++){
         st.insert(arr[i]);       // n log n
     }
 
     int index = 0;
 
-    for(auto it : st)            // Just a loop For set
-    {
-        arr[index] = it;         // storing at that index then incrementing
-        index++;                 // log n
+    for(auto it : st) {         
+        arr[index] = it;         
+        index++;             
     }
-
     return st.size();        
 }
 
-//---------------------------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------
 
 
 // using two pointer approch
-int Optimal(int arr[], int n)
-{
+int Optimal(int arr[], int n){
     int i = 0;
 
-    for(int j = 1; j < n; j++)
-    {
-        if(arr[j] != arr[i])
-        {
+    for(int j = 1; j < n; j++){
+        if(arr[j] != arr[i]){
             i++;
             arr[i] = arr[j];
         }
     }
 
-    return i + 1;
+    return i + 1;          // returning size of an array
 }
 // time-complexity = only j moves once = O(n)
---
+++ ARRAYS/06_leftRotate.cpp
@@ -0,0 +1,11 @@
+// So you need to left rotate an arrays by one places
+// [1,2,3,4,5] => [2,3,4,5,1]
+// Pretty Easy Problem so Directly Jump to the OPTIMAL
+
+#include<iostream>
+#include<set>
+using namespace std;
+
+int optimal(int arr[], int n){
+    
+}
