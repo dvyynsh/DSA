ea10a46d2a7d3e1324f2fec2e9b9c8166435858f
Divyansh Singh <divyansh205044@gmail.com>
2026-08-20 14:06:43 +0530

    Time: 0 ms (100%), Space: 14.3 MB (57.92%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_set<int> st;
+        vector<int> ans;
+
+        // Store all elements of nums1 in the set
+        for (int i = 0; i < nums1.size(); i++) {
+            st.insert(nums1[i]);
+        }
+
+        // Traverse nums2
+        for (int i = 0; i < nums2.size(); i++) {
+             // Check if element exists in the set
+            if (st.find(nums2[i]) != st.end()) {
+                ans.push_back(nums2[i]); 
+                st.erase(nums2[i]);   
+            }
+        }
+    return ans;
+
+    }
+
+};
